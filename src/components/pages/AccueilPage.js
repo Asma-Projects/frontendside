5df4a31c4d81e478c6e37620cbb06d6be337e346
Asma-Projects <38215508+Asma-Projects@users.noreply.github.com>
2018-05-02 14:49:42 +0200

    Add files via upload

--- src/components/pages/AccueilPage.js
+++ src/components/pages/AccueilPage.js
@@ -9,17 +9,15 @@ const body={
 
 }
 
-
 class AccueilPage extends React.Component {
-  
+
   handleUpload(e) {
     e.preventDefault();
     const data = new FormData();
+    
     data.append('file', e.target.files[0]);
-
-    data.append('filename', "test");
-    console.log('in')
-    console.log(e.target.files[0])
+ data.append('filename', "test");
+    
     fetch('http://localhost:8080/upload', { // Your POST endpoint
       method: 'POST',
       body: data
@@ -33,12 +31,32 @@ class AccueilPage extends React.Component {
       success => console.log(success) // Handle the success response object
     ).catch(
       error => null // Handle the error response object
-    );
-  }
+    );}
+
+  onClick(file){
+    const data = new FormData();
+    
+    data.append('file', document.getElementById('pdf').files[0]);
+ data.append('filename', 'test');
+ console.log(file);
+ fetch('http://localhost:8080/convert', { // Your POST endpoint
+      method: 'POST',
+      body : data
+    })
+    .then(
+      response => {
+        console.log(response);
+        return response.json()
+      } // if the response is a JSON object
+    ).then(
+      success => console.log(success) // Handle the success response object
+    ).catch(
+      error => null // Handle the error response object
+    ); }
+
   render() {
     return ( <div style={body} >
     <br/>
-  
     <br/>
     <label><p style={{width:'15em',textAlign: 'center'
     ,fontWeight:'bold',borderRadius:'5px',display:'inline-block',marginTop:'7em',marginLeft:'2em' , backgroundColor:'#D19275' ,padding: '8px 14px',
@@ -46,11 +64,30 @@ class AccueilPage extends React.Component {
       <br/>
       <br/>
       <br/>
-          <form>
-          <input type="file" onChange={this.handleUpload} />
+          <form encType="multipart/form-data" >
+          <input  id="pdf" type="file" onChange={this.handleUpload}   />
           <br/>
           <br/>
             </form>
+            <br/>
+            <br/>
+            <br/>
+            <button style={{display: 'inline-block',
+  padding: '15px 25px' ,
+  fontSize: '13px ',
+  cursor: 'pointer',
+  textAlign: 'center' ,
+  textDecoration: 'none' ,
+  outline: 'none',
+  color: '#800000',
+  backgroundColor: '#D2B48C',
+  border: 'none',
+  borderRadius: '15px',
+  boxShadow: '0 9px #999',
+  transform: 'translateY(4px)'}} onClick={this.onClick}> Convert</button>
+
+
+
             <br/>
             <br/>
             <br/>
@@ -58,8 +95,5 @@ class AccueilPage extends React.Component {
     );
   }
 }
-
-
-
-
 export default AccueilPage ;
+           
